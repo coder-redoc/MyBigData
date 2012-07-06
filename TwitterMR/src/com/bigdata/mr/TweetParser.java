a1019c63d19e564b5204e9e4a31c4b9ad1ef2c28
Satish.Dandu <Satish.Dandu@SJC1SDANDU-L1.mit>
2012-07-06 15:18:14 -0700

    parsing all tweet entities

--- TwitterMR/src/com/bigdata/mr/TweetParser.java
+++ TwitterMR/src/com/bigdata/mr/TweetParser.java
@@ -1,5 +1,10 @@
 package com.bigdata.mr;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.apache.log4j.Logger;
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -11,28 +16,56 @@ import org.json.JSONObject;
  */
 public class TweetParser {
 
-    private static Logger log = Logger.getLogger(TweetParser.class.getName());
+	private static Logger log = Logger.getLogger(TweetParser.class.getName());
 
-    /**
-     * JSON util method to parse url from the tweet
-     * @param tweet
-     * @return
-     * @throws JSONException
-     */
-    public static String parseTwitterUrlData(String tweet) throws JSONException{
+	/**
+	 * JSON util method to parse tweet entities
+	 * @param tweet
+	 * @return
+	 * @throws JSONException
+	 */
+	public static Map<String, String> parseTwitterUrlData(String tweet) throws JSONException{
 
-        String expandedUrl=null;
-        JSONObject myjson = new JSONObject(tweet);
-        JSONObject entitiesObj = myjson.getJSONObject(TweetConstants.ENTITIES);
-        JSONArray urlArray = entitiesObj.getJSONArray(TweetConstants.URL);
-        if(null!=urlArray && urlArray.length()>0){
-            JSONObject urlDataObj = (JSONObject)urlArray.get(0);
-            //String displayUrl = urlDataObj.getString("display_url");
-            expandedUrl = urlDataObj.getString(TweetConstants.EXPANDED_URL);
-        }
+		Map<String, String> tweetEntitiesMap = new HashMap<String, String>();
 
-        return expandedUrl;
-    }
+		JSONObject myjson = new JSONObject(tweet);
+		JSONObject entitiesObj = myjson.getJSONObject(TweetEnums.ENTITIES.toString());
+		JSONArray urlArray = entitiesObj.getJSONArray(TweetEnums.URLS.toString());
+		JSONArray userMentionsArray = entitiesObj.getJSONArray(TweetEnums.USER_MENTIONS.toString());
+		JSONArray hashtagsArray = entitiesObj.getJSONArray(TweetEnums.HASHTAGS.toString());
+
+		//Parse tweet url entities
+		if(null!=urlArray && urlArray.length()>0){
+			JSONObject urlDataObj = (JSONObject)urlArray.get(0);
+			tweetEntitiesMap.put(TweetEnums.URL.toString(),  		urlDataObj.getString(TweetEnums.URL.toString()));
+			tweetEntitiesMap.put(TweetEnums.EXPANDED_URL.toString(), urlDataObj.getString(TweetEnums.EXPANDED_URL.toString()));
+			tweetEntitiesMap.put(TweetEnums.DISPLAY_URL.toString(),  urlDataObj.getString(TweetEnums.DISPLAY_URL.toString()));
+		}
+
+		//Parse tweet user mentions entities
+		if(null!=userMentionsArray && userMentionsArray.length()>0){
+			JSONObject userMentionsObj = (JSONObject)userMentionsArray.get(0);
+			tweetEntitiesMap.put(TweetEnums.USER_ID.toString(),  	userMentionsObj.getString(TweetEnums.USER_ID.toString()));
+			tweetEntitiesMap.put(TweetEnums.SCREEN_NAME.toString(), userMentionsObj.getString(TweetEnums.SCREEN_NAME.toString()));
+			tweetEntitiesMap.put(TweetEnums.NAME.toString(),  		userMentionsObj.getString(TweetEnums.NAME.toString()));
+		}
+
+		//Parse tweet hashtag entities
+		if(null!=hashtagsArray && hashtagsArray.length()>0){
+			List<String> hashTagList = new ArrayList();
+			StringBuffer hashData = new StringBuffer();
+			for(int i=0;i<hashtagsArray.length(); i++){
+				JSONObject hashTagsObj = (JSONObject)hashtagsArray.get(i);
+				hashData.append("#");
+				hashData.append(hashTagsObj.getString(TweetEnums.HASHTAG_TEXT.toString()));
+				if(!(i+1==hashtagsArray.length()))
+					hashData.append(",");
+			}
+			tweetEntitiesMap.put(TweetEnums.HASHTAG_DISPLAY_KEY.toString(), hashData.toString());
+		}
+
+		return tweetEntitiesMap;
+	}
 
 }
 
