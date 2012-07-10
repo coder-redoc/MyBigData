4ae7ac8834536355c3dcac5ed4fadd18d9424b7e
Satish.Dandu <Satish.Dandu@SJC1SDANDU-L1.mit>
2012-07-10 16:51:32 -0700

    Script to track tweets

--- TwitterMR/scripts/tweetTracker.sh
+++ TwitterMR/scripts/tweetTracker.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-USER_NAME=$1
-PASSWORD=$2
-TWEETS_OUTPUT_FILENAME=$3
-
 if [ $# -lt 3 ] ; then
 echo ""
   echo "Twitter Tracking KeyWords using Twitter Streaming API"
   echo "----------------------------------------------"
-  echo "<USAGE>: tweetTracker.sh USER_NAME PASSWORD TWEETS_OUTPUT_FILE"
+  echo "<USAGE>: tweetTracker.sh USER_NAME PASSWORD DIR_TO_STORE_TWEETS<ABSOLUTE_PATH>"
   echo ""
   exit 1
 fi
 
-echo "UserName=$USER_NAME, Password=$PASSWORD, TweetsOutputFile=$TWEETS_OUTPUT_FILENAME"
+USER_NAME=$1
+PASSWORD=$2
+TWEETS_OUTPUT_DIR=$3
 
+echo "UserName=$USER_NAME, Password=$PASSWORD"
+
+mkdir -p $TWEETS_OUTPUT_DIR
 
 TRACK_WORDS="http,https"
-
-curl -d 'track=http,https' https://stream.twitter.com/1/statuses/filter.json -u$USER_NAME:$PASSWORD >> $TWEETS_OUTPUT_FILE
+curl -d track=$TRACK_WORDS https://stream.twitter.com/1/statuses/filter.json -u$USER_NAME:$PASSWORD >> $TWEETS_OUTPUT_DIR/tweets.json 
